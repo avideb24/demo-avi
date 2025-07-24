1515ee66e8ee2631aa6ccb6caf5243c1ccc5e3ca
avideb24 <debavi24@gmail.com>
2025-07-24 12:47:37 +0600

    uu

--- src/components/ClientRoot/ClientRoot.jsx
+++ src/components/ClientRoot/ClientRoot.jsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-// import { Profile } from "@/components/cards/Profile/Profile";
-// import MobileButtons from "@/components/mobile/mobile-buttons";
+// import dynamic from 'next/dynamic';
+// const Profile = dynamic(() => import('@/components/cards/Profile/Profile'), { ssr: false });
+// const MobileButtons = dynamic(() => import('@/components/mobile/mobile-buttons'), { ssr: false });
+// const MobileCardModal = dynamic(() => import('@/components/mobile/mobile-card-modal'), { ssr: false });
+// const Loading = dynamic(() => import('@/components/cards/Profile/Profile'), { ssr: false });
+
 import GlobalProvider from "@/components/provider/GlobalProvider";
-// import MobileCardModal from "@/components/mobile/mobile-card-modal";
-// import Loading from "@/app/loading";
+import Loading from "@/app/loading";
 
 export default function ClientRoot({ children }) {
   
@@ -20,12 +23,12 @@ export default function ClientRoot({ children }) {
   return (
     <GlobalProvider>
       layout
-      {/* {loading && (
+      {loading && (
         <div className="fixed top-0 left-0 w-full h-full z-50 bg-primary overflow-hidden">
           <Loading />
         </div>
       )}
-
+{/* 
       <div className={`transition-opacity duration-500 ease-in-out ${loading ? "opacity-0" : "opacity-100"}`}>
         <div className="w-screen h-screen lg:px-4 lg:pt-4 lg:grid lg:grid-cols-3 lg:gap-4">
           <Profile />
